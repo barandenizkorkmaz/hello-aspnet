4ca2390c08f16c83a63c62b0f726a038a102413b
barandenizkorkmaz <barandenizkorkmaz@gmail.com>
2024-02-28 23:43:06 +0100

    feat: ✨ update src files

--- src/_3_HTTP/HTTP/Program.cs
+++ src/_3_HTTP/HTTP/Program.cs
@@ -5,8 +5,13 @@ app.Run(async (HttpContext context) =>
 {
     // References: https://developer.mozilla.org/en-US/docs/Web/HTTP
     
-    // Status Codes
-    context.Response.StatusCode = 400;
+    // Request Header
+    string path = context.Request.Path;
+    string method = context.Request.Method;
+    
+    
+    // Status Codes: Response
+    context.Response.StatusCode = 200;
     
     // Response Header
     context.Response.Headers["Team"] = "Besiktas";
@@ -16,7 +21,10 @@ app.Run(async (HttpContext context) =>
     // Response Body
     await context.Response.WriteAsync("<h1>Hello</h1>");
     await context.Response.WriteAsync("<h2>HTTP!</h2>");
-    
+    await context.Response.WriteAsync($"<h3>Url: {path}</h3>");
+    await context.Response.WriteAsync($"<h3>Method: {method}</h3>");
+    // try: localhost:5097/ and localhost:5097/hello and see the difference!
+
 });
 
 app.Run();
