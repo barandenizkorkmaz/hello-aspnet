2ea7026b5008b3e2d1bc181b62d9ec3df8d036a5
barandenizkorkmaz <barandenizkorkmaz@gmail.com>
2024-02-28 23:28:39 +0100

    feat: ✨ update src files

--- src/_3_HTTP/HTTP/Program.cs
+++ src/_3_HTTP/HTTP/Program.cs
@@ -3,9 +3,19 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
+    // References: https://developer.mozilla.org/en-US/docs/Web/HTTP
+    
+    // Status Codes
     context.Response.StatusCode = 400;
-    await context.Response.WriteAsync("Hello");
-    await context.Response.WriteAsync(" HTTP!");
+    
+    // Response Header
+    context.Response.Headers["Team"] = "Besiktas";
+    // Overwrite existing `Server` header
+    context.Response.Headers["Server"] = "Not Kestrel";
+    context.Response.Headers["Content-Type"] = "text/html";
+    // Response Body
+    await context.Response.WriteAsync("<h1>Hello</h1>");
+    await context.Response.WriteAsync("<h2>HTTP!</h2>");
     
 });
 
